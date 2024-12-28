461d8091b1c76ddf4f6b6d0a48a951170c238348
thanhsang270693 <thanhsang27061993@gmail.com>
2024-12-29 00:47:15 +0700

    script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 document.querySelector('form').addEventListener('submit', function(event) {
   const name = document.getElementById('name').value;
   const phone = document.getElementById('phone').value;
+console.log("Chuyển hướng tới trang danh sách đăng ký...");
+window.location.href = "danh-sach-dang-ky.html";
 
   if (!name || !phone) {
     alert('Vui lòng điền đầy đủ thông tin!');
